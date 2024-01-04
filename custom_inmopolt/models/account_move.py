1fca866ad8b25d5bdba0f1e5238b7efd5a70db0d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-04 13:01:58 +0100

    auto reconcile

--- custom_inmopolt/models/account_move.py
+++ custom_inmopolt/models/account_move.py
@@ -7,7 +7,7 @@ class AccountMove(models.Model):
     def inmopolt_auto_reconcile_paid_invoices(self):
         # Marcar como pagadas los recibos y facturas si la deuda es nula (evitar reconciliar con el banco):
         for record in self:
-            if (record.move_type in ['in_invoice','in_refund','out_invoice','out_refund'])
+            if (record.move_type in ['in_invoice','in_refund','out_invoice','out_refund']):
                 and (record.payment_state == 'in_payment')
                 and (record.amount_residual == 0):
                     record['payment_state'] = 'paid'
